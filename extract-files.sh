a46f2a0e48427ccf4c656ebcb6bc74a0fc29b6ac
Bruno Martins <bgcngm@gmail.com>
2018-06-19 23:55:25 +0100

    redfin: Fix typo from CodeAurora in blob_fixup
    * Let com.qualcomm.qcrilmsgtunnel break free
    
    Change-Id: Ide5759eef7e3100ac5b50f5dc95a3f90119e592f

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,10 @@ function blob_fixup() {
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
+    # Fix typo in qcrilmsgtunnel whitelist
+    product/etc/sysconfig/nexus.xml)
+        sed -i 's/qulacomm/qualcomm/' "${2}"
+        ;;
     esac
 }
 
