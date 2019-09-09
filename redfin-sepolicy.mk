02a5f5a107f415e4674942ed6b81f79643f19669
ChengYou Ho <chengyouho@google.com>
2019-09-09 16:27:07 +0800

    Update to latest and move common sepolicy
    to common folder
    
    Bug: 140363359
    Bug: 141538370
    (cherry picked from commit b1f90577339a11dcfa8c38cbb2e2943bfc6166fa)
    
    Change-Id: I9cbcc1111fcdf38794b4e1e1bc5f9d9042354f02

--- redfin-sepolicy.mk
+++ redfin-sepolicy.mk
@@ -1,8 +1,2 @@
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/redfin-sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/redfin-sepolicy/private
-
 # vendors
 BOARD_SEPOLICY_DIRS += device/google/redfin-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/redfin-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/redfin-sepolicy/vendor/qcom/sm7250
-
