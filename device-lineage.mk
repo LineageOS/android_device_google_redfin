2a34d2619e5065c7409630384d0ae49f094ee37e
Danny Lin <danny@kdrag0n.dev>
2020-11-02 19:43:49 -0800

    redfin: Enable refresh rate toggle in Settings
    This device supports switching between 60 and 90 Hz refresh rates, so
    expose it in Settings -> Display -> Smooth Display for users to save
    battery if necessary.
    
    Test: visual confirmation after toggling several times
    Change-Id: Ie698ec4d4e738afd2a9055dba2369233103a4f13

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
+
 $(call inherit-product, device/google/redbull/device-lineage.mk)
 
 $(call inherit-product-if-exists, vendor/google/redfin/redfin-vendor.mk)
