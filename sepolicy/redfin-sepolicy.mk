26a7c2ae35dcb491b5d999bbc2c2fa346d427854
Alessandro Astone <ales.astone@gmail.com>
2021-01-09 11:47:02 +0100

    redfin: Update sepolicy path
    Change-Id: Ibae8f5d533d78ff364e6d5eecaea6b9fada41a26

--- sepolicy/redfin-sepolicy.mk
+++ sepolicy/redfin-sepolicy.mk
@@ -1,3 +1,3 @@
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/redfin-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/redfin-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/redfin/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/redfin/sepolicy/tracking_denials
