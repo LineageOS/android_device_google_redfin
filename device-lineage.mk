4fe0755476c34e955bf7464c3ff5af4943514a47
Alessandro Astone <ales.astone@gmail.com>
2021-01-30 00:55:01 +0100

    redfin: Build touch hal
    Change-Id: I0c0cd1f14520c7433e8fb1240f18579596e5f56c

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,7 @@
 $(call inherit-product, device/google/redbull/device-lineage.mk)
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.redfin
+
 $(call inherit-product-if-exists, vendor/google/redfin/redfin-vendor.mk)
