c6d251d7d48cb2ce6798480218df799d2eea14a3
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    redfin: Implement PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,4 +9,10 @@ BOOT_KERNEL_MODULES += sec_touch.ko
 
 include device/google/redbull/BoardConfigLineage.mk
 
+# Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/redfin/lineage_compatibility_matrix.xml
+
+# SEPolicy
+BOARD_SEPOLICY_DIRS += device/google/redfin/sepolicy-lineage/vendor
+
 include vendor/google/redfin/BoardConfigVendor.mk
