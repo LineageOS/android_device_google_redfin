5bd2aa15286bc4a53e5891596dc284c3979a9055
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    redfin: Build PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- device-lineage.mk
+++ device-lineage.mk
@@ -8,3 +8,6 @@ $(call inherit-product, device/google/redbull/device-lineage.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
+
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
