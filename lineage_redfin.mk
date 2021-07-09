9ef0aca3f0e02f572221d1ca07d655c1fe3725c7
Alessandro Astone <ales.astone@gmail.com>
2021-07-09 11:24:36 +0200

    redfin: RQ3A.210705.001
    Change-Id: I701bc3ad04eaf0e3c80ca4c05aedefb25995925d

--- lineage_redfin.mk
+++ lineage_redfin.mk
@@ -34,6 +34,6 @@ TARGET_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
