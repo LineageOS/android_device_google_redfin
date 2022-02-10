64e832f6434f0328574e3afba17b728ae8e30486
Michael Bestas <mkbestas@lineageos.org>
2022-02-10 23:03:46 +0200

    redfin: Use redbull kernel source
    Change-Id: I9636748cb54e175f471b0d55c5c4afa5de703df6

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/google/redbull
+
 # Add before redbull BoardConfigLineage.mk
 BOOT_KERNEL_MODULES += sec_touch.ko
 
