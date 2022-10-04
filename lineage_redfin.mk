c2b7d76fb9fbc70cd4a24633a8615f4bdbe4d354
Michael Bestas <mkbestas@lineageos.org>
2022-10-04 17:30:04 +0300

    Update fingerprint/build description from TP1A.221005.002
    Change-Id: I3025530461cd9d950f5304a5212820e564665c62

--- lineage_redfin.mk
+++ lineage_redfin.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 13 TP1A.220905.004 8927612 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 13 TP1A.221005.002 9012097 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:13/TP1A.220905.004/8927612:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:13/TP1A.221005.002/9012097:user/release-keys
 
 $(call inherit-product, vendor/google/redfin/redfin-vendor.mk)
