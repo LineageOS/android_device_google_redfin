5fb5eacf643f06e27b48c98223d1e1fe258df1c7
Michael Bestas <mkbestas@lineageos.org>
2023-09-25 18:53:03 +0300

    Update fingerprint/build description from TQ3A.230901.001
    Change-Id: I95288db2fcaf61656c5b047872130b4283944068

--- lineage_redfin.mk
+++ lineage_redfin.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 13 TQ3A.230805.001 10316531 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 13 TQ3A.230901.001 10750268 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:13/TQ3A.230805.001/10316531:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:13/TQ3A.230901.001/10750268:user/release-keys
 
 $(call inherit-product, vendor/google/redfin/redfin-vendor.mk)
