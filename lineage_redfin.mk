eb6388c1c8123663e4b5621611c250f0a8795143
Michael Bestas <mkbestas@lineageos.org>
2024-02-07 19:14:16 +0200

    Update fingerprint/build description from UP1A.231105.001.B2
    Change-Id: If7ff8ebd941f1edac0a2c121f75b7e728256bfad

--- lineage_redfin.mk
+++ lineage_redfin.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=redfin \
-    PRIVATE_BUILD_DESC="redfin-user 14 UP1A.231105.001 10817346 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 14 UP1A.231105.001.B2 11260668 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:14/UP1A.231105.001/10817346:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:14/UP1A.231105.001.B2/11260668:user/release-keys
 
 $(call inherit-product, vendor/google/redfin/redfin-vendor.mk)
