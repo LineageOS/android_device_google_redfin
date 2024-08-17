77c402131562915ce1b8c916b436b2b22712e61a
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    redfin: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device-lineage.mk
+++ device-lineage.mk
@@ -9,21 +9,12 @@ $(call inherit-product, device/google/redbull/device-lineage.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
 
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.device-V2-ndk.vendor:64 \
-    android.hardware.camera.provider-V2-ndk.vendor:64
-
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
 
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
-
-# Wi-Fi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi-V2-ndk.vendor:64
 
 # wireless_charger HAL service
 include device/google/redfin/wireless_charger/wireless_charger.mk
