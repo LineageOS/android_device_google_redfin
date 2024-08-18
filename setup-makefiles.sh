4d16c456689b69bfd08a13b27215922160492f15
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    redfin: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -50,6 +50,7 @@ function lib_to_package_fixup_vendor_variants() {
         libprotobuf-cpp-lite-21.12)
             echo "libprotobuf-cpp-lite"
             ;;
+        libwpa_client) ;;
         *)
             return 1
             ;;
