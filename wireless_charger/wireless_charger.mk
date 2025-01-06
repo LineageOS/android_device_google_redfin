54ff2cc4b949481dc60318f15b3cc6c34d6283e6
Michael Bestas <mkbestas@lineageos.org>
2025-01-06 04:16:33 +0200

    redfin: Remove non existent soong namespaces
    Change-Id: If0b86441d47da127e37aeed6fcd6cbfff656e3f8

--- wireless_charger/wireless_charger.mk
+++ wireless_charger/wireless_charger.mk
@@ -1,5 +1,4 @@
 # This wireless_charger folder is used to keep the compatibility for later google devices
-PRODUCT_SOONG_NAMESPACES += vendor/google/interfaces
 PRODUCT_PACKAGES += vendor.google.wireless_charger-default
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/redfin/wireless_charger/compatibility_matrix.xml
 
