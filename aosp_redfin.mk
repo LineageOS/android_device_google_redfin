5de85b9cc8522562b97317713fea3719b2b27fc6
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-16 01:26:53 -0500

    redfin: Relax artifact requirement
    * We have a few deps on system that can't be worked around easily.
    * See: ecoservice stuff in hardware/google/pixel.
    
    Change-Id: I2208ced0fb3b8a56847fc5329517ffacef7861b9

--- aosp_redfin.mk
+++ aosp_redfin.mk
@@ -25,7 +25,7 @@ endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
 
 # Enable mainline checking
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
 #
 # All components inherited here go to system_ext image
